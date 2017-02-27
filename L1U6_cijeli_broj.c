9bf96cf54326554fee46aa35de1187535ca45d8b
Mario Krajacic <krajacic.mario@gmail.com>
2017-02-27 15:52:11 +0100

    Add files via upload

---
+++ L1U6_cijeli_broj.c
@@ -0,0 +1,19 @@
+/*Provjera je li upisan cijeli broj*/
+
+#include <stdio.h>
+
+int main(void)
+{
+	int broj;
+	printf("Unesite jedan broj\n");
+	scanf("%d", &broj);
+
+	if(broj>=0)
+		printf("Da\n");
+	else
+		printf("Ne\n");
+
+	getchar();
+	getchar();
+	return 0;
+}
