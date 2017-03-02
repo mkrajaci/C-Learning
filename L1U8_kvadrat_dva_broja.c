016a8013da369cf9e5b513fa24e8e954b0b4d86a
Mario Krajacic <krajacic.mario@gmail.com>
2017-03-02 17:03:59 +0100

    Update L1U8_kvadrat_dva_broja.c

--- L1U8_kvadrat_dva_broja.c
+++ L1U8_kvadrat_dva_broja.c
@@ -1,4 +1,4 @@
-/*Izraèun kvadrata dva broja*/
+/*Izracun kvadrata dva broja*/
 
 #include <stdio.h>
 
@@ -6,9 +6,9 @@ int main(void)
 {
 	int broj;
 
-	printf("Molim vas upišite jedan broj:\n");
+	printf("Molim vas upiÅ¡ite jedan broj:\n");
 	scanf_s("%d", &broj);
-									/*Ako je broj pozitivan ispisati æe dupli broj od unesenog, a ako je negativan onda kvadrat, za nulu - nula*/
+									/*Ako je broj pozitivan ispisati Ã¦e dupli broj od unesenog, a ako je negativan onda kvadrat, za nulu - nula*/
 	if (broj > 0)
 		printf("Dupli broj od upisanog %d je: %d", broj, broj*2);
 	else if (broj < 0)
