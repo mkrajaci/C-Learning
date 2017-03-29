16198840af81f8614c552c588cb117f1bfb21e57
Mario Krajacic <krajacic.mario@gmail.com>
2017-03-29 15:56:16 +0200

    Add files via upload

---
+++ L5Z1.c
@@ -0,0 +1,25 @@
+/*Napisati funkciju èiji je prototip: int NulBitova(char znak).
+Funkcija treba prebrojiti koliko je bitova u ulaznom znaku postavljeno na 0.
+Npr. znak 'A' ima ASCII vrijednost 65, a to je 01000001 u binarnom zapisu. Funkcija bi za
+znak 'A' dakle trebala vratiti 6 jer je toliko nula u binarnom prikazu tog znaka.
+Napomena: koristiti bitovne operatore.*/
+
+#include <stdio.h>
+
+int main(void)
+{
+	char znak;
+	int znak_ascii;
+
+	printf("Upisite jedan znak: ");
+	scanf_s(" %c", &znak);
+	znak_ascii = znak;
+
+	printf("%d", znak_ascii);
+
+
+	getchar();
+	getchar();
+
+	return 0;
+}
