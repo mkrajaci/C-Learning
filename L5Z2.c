7af49d44d3f731b006b657534e98362f23f7c36f
Mario Krajacic <krajacic.mario@gmail.com>
2017-03-29 12:37:55 +0200

    Add files via upload

--- L5Z2.c
+++ L5Z2.c
@@ -4,34 +4,51 @@ Primjer: broj 13597 treba pretvoriti u brojeve 157 i 39.
 Funkcija brojeve treba vratiti glavnom programu koji ih onda ispisuje.*/
 
 #include <stdio.h>
+#include <math.h>
 
-int funkcija_za_povrat_brojeva();
+int funkcija_za_povrat_brojeva(int uneseni_broj);
 
 int main(void)
 {
-	int uneseni_broj, znamenka, brojac, djeljiv = 0, nije_djeljiv = 0;
-	/*
+	int uneseni_broj;
+
 	printf("Unesite jedan cijeli broj: ");
 	scanf_s("%d", &uneseni_broj);
-	*/
-	uneseni_broj = 13597;
 
-	for (brojac = 0; brojac <= uneseni_broj; ++brojac)
-	{
-		znamenka = uneseni_broj % 10;
-		uneseni_broj /= 10;
-		
-		if (znamenka % 3 == 0)
-			djeljiv = znamenka;
-		else
-			nije_djeljiv = znamenka;
-
-		printf("%d ", djeljiv);
-		printf("%d ", nije_djeljiv);
-	}
+	printf("%d", funkcija_za_povrat_brojeva(uneseni_broj));
+	
 
 	getchar();
 	getchar();
 
 	return 0;
+}
+	
+
+int funkcija_za_povrat_brojeva(int uneseni_broj)
+{
+	int znamenka, brojac, brojac_djeljiv = 0, brojac_nije_djeljiv = 0, djeljiv = 0, nije_djeljiv = 0;
+
+	brojac = 0;
+	while (uneseni_broj)
+	{
+		znamenka = uneseni_broj % 10;
+		uneseni_broj /= 10;
+
+		if (znamenka % 3 == 0)
+		{
+			djeljiv = djeljiv + (pow(10, brojac_djeljiv) * znamenka);
+			++brojac_djeljiv;
+		}
+		else
+		{
+			nije_djeljiv = nije_djeljiv + (pow(10, brojac_nije_djeljiv) * znamenka);
+			++brojac_nije_djeljiv;
+		}
+		++brojac;
+
+	}
+
+	return djeljiv;
+	return nije_djeljiv;
 }
