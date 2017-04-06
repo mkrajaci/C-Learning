69b33a1b2d50adff2126a1a33344ffff75375e82
Mario Krajacic <krajacic.mario@gmail.com>
2017-04-06 22:33:07 +0200

    Add files via upload

--- L6Z3.c
+++ L6Z3.c
@@ -1,18 +1,28 @@
-/*Konzervativni nizovi su oni nizovi cijelih brojeva koji poèinju s 1, 
+/*Konzervativni nizovi su oni nizovi cijelih brojeva koji poèinju s 1,
 a svaki iduæi èlan im je ili toèno za jedan veæi od njemu direktno prethodnog
 ili je identièan nekom (bilo kojem) prethodnom èlanu niza.
 Npr. niz: 1, 1, 2, 3, 4, 2, 4 jest konzervativan
-dok niz: 1, 2, 3, 3, 1, 2, 4, 5 nije jer èlan s vrijednošæu 4 nije niti za jedan veæi od njemu direktno prethodnog 
+dok niz: 1, 2, 3, 3, 1, 2, 4, 5 nije jer èlan s vrijednošæu 4 nije niti za jedan veæi od njemu direktno prethodnog
 (2) niti se ranije pojavljivao u nizu.
 Napišite funkciju koja prima jedan niz cijelih brojeva te vrati 1 ako je on konzervativan, a 0 inaèe.*/
 #include <stdio.h>
-
+int funkcija_je_li_konzervativan_niz(int *niz, int duljina);
 int main(void)
 {
-	int niz[7] = { 1, 1, 2, 3, 4, 2, 4 };
-	
-	funkcija_je_li_konzervativan_niz(niz, 7);
-
+	int niz[] = { 1, 1, 2, 3, 4, 2, 4 };
+	int koliko_je_brojeva_u_nizu = sizeof(niz) / sizeof(niz[0]);	
+	if (funkcija_je_li_konzervativan_niz(niz, koliko_je_brojeva_u_nizu))
+		printf("Konzervativan");
+	else
+		printf("Nije konzervativan");
+/*
+	int niz[] = { 1, 2, 4, 5 };
+	int koliko_je_brojeva_u_nizu = sizeof(niz) / sizeof(niz[0]);
+	if (funkcija_je_li_konzervativan_niz(niz, koliko_je_brojeva_u_nizu))
+		printf("Konzervativan");
+	else
+		printf("Nije konzervativan");
+*/
 	getchar();
 	getchar();
 
@@ -20,6 +30,31 @@ int main(void)
 }
 int funkcija_je_li_konzervativan_niz(int *niz, int duljina)
 {
-
-
+	int konzervativan = 0;
+	int brojac1 = 1;
+	if (niz[0] != 1)
+	{
+		return 0;
+	}
+	while (brojac1 < duljina)
+	{
+		if (!(niz[brojac1] - 1 == niz[brojac1 - 1]))
+		{
+			int brojac2 = 0;
+			while (brojac2 < brojac1)
+			{
+				konzervativan = 0;
+				if (niz[brojac2] == niz[brojac1])
+				{
+					konzervativan = 1;
+					break;
+				}
+				++brojac2;
+			}
+			if (konzervativan == 0)
+			return 0;
+		}		
+		++brojac1;
+	}
+	return 1;
 }
