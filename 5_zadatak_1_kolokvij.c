7e82c8e4286490a4a05b242e1db3d89f0c48dde1
Mario Krajacic <krajacic.mario@gmail.com>
2017-04-17 20:58:58 +0200

    Add files via upload

---
+++ 5_zadatak_1_kolokvij.c
@@ -0,0 +1,32 @@
+﻿/*a) funkciju kvadratna_sredina koja primi tri parametra a, b i c, te kao povratnu
+vrijednost vrati njihovu kvadratnu sredinu ( K=√a2+b2+c2 )
+b) funkciju usredini koja primi tri parametra a, b i c, izracuna njihovu kvadratnu sredinu
+koristeci funkciju iz a) dijela zadatka, te postavi sva tri parametra na dobiveni iznos (parametri
+predstavljaju reference na vanjske varijable).*/
+#include <stdio.h>
+#include <math.h>
+double kvadratna_sredina(int a, int b, int c);
+void u_sredini(int *a, int *b, int *c);
+int main(void)
+{
+	int a, b, c;
+	printf("Molim unesite tri vrijednosti: ");
+	scanf("%d %d %d", &a, &b, &c);
+	u_sredini(a, b, c);
+	printf("%d %d %d", a, b, c);
+	getchar();
+	getchar();
+	return 0;
+}
+void u_sredini(int *a, int *b, int *c)
+{
+	kvadratna_sredina(a, b, c);
+	a = b = c = kvadratna_sredina;
+	return 0;
+}
+double kvadratna_sredina(int *a, int *b, int *c)
+{
+	double kvadratna_sredina;
+	kvadratna_sredina = sqrt(pow(*a, 2) + pow(*b, 2) + pow(*c, 2));
+	return kvadratna_sredina;
+}
---
+++ 6_zadatak_1_kolokvij.c
@@ -0,0 +1,7 @@
+/*Ekvilibrij niza je svaki onaj indeks N koji dijeli niz u dvije polovice (od indeksa 0 do N - 1
+odnosno od indeksa N + 1 do kraja niza) èije su sume èlanova identiène. Npr., u nizu
+0 -3 5 -4 -2 3 1 0
+ekvilibriji se nalaze na indeksima 0, 3 i 7 (primjetite da ekvilibriji mogu biti i na prvom i na zadnjem
+indeksu ukoliko ostatak niza kao sumu daje 0).
+Napišite funkciju koja primi jedan niz nepredznaèenih cijelih brojeva i jedan indeks, te vrati 1 ukoliko je
+taj indeks ekvilibrij niza, a 0 inaèe.*/
