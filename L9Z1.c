b797be88fbbba8a08d45b0be12fa807b03e1c35b
Mario Krajacic <krajacic.mario@gmail.com>
2017-05-03 12:40:37 +0200

    Add files via upload

--- L9Z1.c
+++ L9Z1.c
@@ -1,19 +1,17 @@
 /*Napisati funkciju koja æe u zadanom stringu zamijeniti sva velika slova sa sluèajno odabranim malim slovima.
-Napisati funkciju koja primi string te odredi koje se slovo najèešæe pojavljuje unutar stringa te koja vraæa to slovo. 
+Napisati funkciju koja primi string te odredi koje se slovo najèešæe pojavljuje unutar stringa te koja vraæa to slovo.
 Ukoliko se više slova pojavljuje jednak broj puta vratiti ono koje se nalazi prije u abecednom poretku.
-Uèitati string od korisnika te ga uz pomoæ kreiranih funkcija izmijeniti (ispisati ga nakon izmjene) 
+Uèitati string od korisnika te ga uz pomoæ kreiranih funkcija izmijeniti (ispisati ga nakon izmjene)
 te ispisati koje se slovo najèešæe pojavljuje.*/
-//prvo zamjeni velika slova malima, pa onda dalje.
-
 #include <stdio.h>
 #include <string.h>
+#include <ctype.h>
 void funkcija_zamjene_slova(char *niz);
 char funkcija_najcesce_slovo(char *niz);
 int main(void)
 {
-	char slovo;
 	char niz[] = "OvO Je NeKa ReCENIca";
-//	gets(niz);
+	//	gets(niz);
 	puts(niz);
 	printf("Nakon izmjene: ");
 	funkcija_zamjene_slova(niz);
@@ -29,31 +27,25 @@ void funkcija_zamjene_slova(char *niz)
 	int brojac_niza = 0;
 	for (; niz[brojac_niza] != '\0'; ++brojac_niza)
 	{
-		for (brojac_slova = 'A'; brojac_slova <= 'Z'; ++brojac_slova)
-		{
-			if (niz[brojac_niza] == brojac_slova)
-			{
-				niz[brojac_niza] = 'a' + (rand() % 26);	//random mala slova
-				break;
-			}
-		}
+		if(isupper (niz[brojac_niza]))
+			niz[brojac_niza] = 'a' + (rand() % 26);	//random mala slova
 	}
 }
 char funkcija_najcesce_slovo(char *niz)
 {
 	char najcesce_slovo = 0;
 	int brojac_slova = 0;
-	int brojac_niza = 0;
-	int duzina_stringa;
-	duzina_stringa = strlen(niz);
-	for (; niz[brojac_niza] != '\0'; ++brojac_niza)
-	{	
-		brojac_slova = duzina_stringa;
-		for (; brojac_niza <= brojac_slova; --brojac_slova)
+	for (; *niz != '\0'; ++niz)
+	{
+		if (!isspace(*niz))
 		{
-			if (niz[brojac_niza] == niz[brojac_slova])
+			char *pokazivac = "abcdefghijklmnoprstuvzyx";
+			for (; *pokazivac != '\0'; ++pokazivac)
 			{
-				najcesce_slovo = niz[brojac_niza];
+				if (*pokazivac == *niz)
+				{
+					//ovdje dobijem slovo koje se ponavlja, sto dalje
+				}
 			}
 		}
 	}
