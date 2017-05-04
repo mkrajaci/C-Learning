c54817e0a3d14ec21dd0e6a89a6d83e071b7de25
Mario Krajacic <krajacic.mario@gmail.com>
2017-05-04 12:22:59 +0200

    Add files via upload

--- L9Z3.c
+++ L9Z3.c
@@ -1,18 +1,19 @@
-/*Otmièar želi sastaviti pismo u kojem traži otkupninu koristeæi slova iz izrezanog novinskog èlanka. 
-Napišite funkciju koja æe primiti tekst pisma i tekst èlanka te vratiti 1 ukoliko je pismo moguæe sastaviti koristeæi slova iz danog èlanka, inaèe 0.
-Pri sastavljanju pisma dopušteno je koristiti i VELIKA i mala slova iz èlanka pa je tako npr.
+/*Otmicar zeli sastaviti pismo u kojem trazi otkupninu koristeci slova iz izrezanog novinskog clanka.
+Napisite funkciju koja ce primiti tekst pisma i tekst clanka te vratiti 1 ukoliko je pismo moguce sastaviti koristeci slova iz danog clanka, inace 0.
+Pri sastavljanju pisma dopusteno je koristiti i VELIKA i mala slova iz clanka pa je tako npr.
 tekst:
 "Zelis li svog zeca natrag donesi Milku u pet iza benzinske"
-dopušteno sastaviti i kao:
+dopusteno sastaviti i kao:
 "zELis li svoG ZEca naTRaG donesi milku u Pet IzA bEnziNSke"
-Pretpostavite da se u tekstu pisma koriste iskljuèivo slova engleske abecede.*/
+Pretpostavite da se u tekstu pisma koriste iskljucivo slova engleske abecede.*/
 #include <stdio.h>
 #include <string.h>
+#include <ctype.h>
 char funkcija(char *pismo, char *clanak);
 int main(void)
 {
 	char pismo[] = "Zelis li svog zeca natrag donesi Milku u pet iza benzinske";
-	char clanak[] = "zelisvogzcantrgdmkupbk";			//slova potrebna za pismo
+	char clanak[] = "Zelis li svog zeca natrag donesi Milku u pet iza benzinske";			//slova potrebna za pismo
 //	char clanak[] = "zel";								//slova nedostatna za pismo
 	printf("%d", funkcija(pismo, clanak));
 	getchar();
@@ -21,13 +22,25 @@ int main(void)
 }
 char funkcija(char *pismo, char *clanak)
 {
-	char *novi_niz;
 	int brojac;
-	novi_niz = strtok(pismo, " ");
-	for (brojac = 0; novi_niz != NULL; ++brojac)
+	int ima_slovo;
+	for (; *pismo != '\0'; ++pismo)
 	{
-		
-
+		if (!isspace(*pismo))
+		{
+			ima_slovo = 0;
+			for (brojac = 0; clanak[brojac] != '\0'; ++brojac)
+			{
+				if (*pismo == clanak[brojac])
+				{
+					clanak[brojac] = '!';
+					++ima_slovo;
+					break;
+				}
+			}
+			if (!ima_slovo)
+				return 0;
+		}
 	}
-	
+	return 1;
 }
