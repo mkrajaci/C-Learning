ecb64b092f7ba2928322a97ac47bfd4fed59264c
Mario Krajacic <krajacic.mario@gmail.com>
2017-05-13 11:05:45 +0200

    Add files via upload

--- L10Z3.c
+++ L10Z3.c
@@ -1,69 +1,64 @@
 /*struct datoteka {
-char naziv[128];
-unsigned velicina;
-};
-struct folder {
-char naziv[128];
-struct folder *folderi;
-int broj_foldera;
-struct datoteka *datoteke;
-int broj_datoteka;
-};
-Uz zadane definicije struktura datoteka i folder napiši funkciju koja æe primiti pokazivaè
-na jedan folder te ispisati pune nazive svih datoteka s ekstenzijom „jpg“ koje se u njemu
-nalaze. Povratna vrijednost funkcije je ukupna velièina svih JPG datoteka u tom folderu.
-Uputa: napunis si niz struktura ruèno (zaharkodiraš), ne biras si folder s diska jer nismo radili šetanje po file Exploreru (niti æemo)*/
+ char naziv[128];
+ unsigned velicina;
+ };
+ struct folder {
+ char naziv[128];
+ struct folder *folderi;
+ int broj_foldera;
+ struct datoteka *datoteke;
+ int broj_datoteka;
+ };
+ Uz zadane definicije struktura datoteka i folder napiÅ¡i funkciju koja ce primiti pokazivac
+ na jedan folder te ispisati pune nazive svih datoteka s ekstenzijom â€žjpgâ€œ koje se u njemu
+ nalaze. Povratna vrijednost funkcije je ukupna velicina svih JPG datoteka u tom folderu.
+ Uputa: napunis si niz struktura rucno (zaharkodiras), ne biras si folder s diska jer nismo radili setanje po file Exploreru (niti cemo)*/
 #include <stdio.h>
 #include <string.h>
-struct datoteka 
+struct datoteka
 {
-	char naziv[128];
-	unsigned velicina;
+    char naziv[128];
+    unsigned velicina;
 };
-struct folder 
+struct folder
 {
-	char naziv[128];
-	struct folder *folderi;
-	int broj_foldera;
-	struct datoteka *datoteke;
-	int broj_datoteka;
+    char naziv[128];
+    struct folder *folderi;
+    int broj_foldera;
+    struct datoteka *datoteke;
+    int broj_datoteka;
 };
-double velicina_slike(struct folder*);
-int main(void) 
+double moja_funkcija(struct folder *mapa);
+int main(void)
 {
-	double velicina;
-	struct datoteka datoteka[4];
-	strcpy(datoteka[0].naziv, "slika1.jpg");
-	strcpy(datoteka[1].naziv, "slika2.jpg");
-	strcpy(datoteka[2].naziv, "slika3.jpg");
-	strcpy(datoteka[3].naziv, "slika4.jpg");
-	datoteka[0].velicina = 10;
-	datoteka[1].velicina = 10;
-	datoteka[2].velicina = 10;
-	datoteka[3].velicina = 10;
-	struct folder folder;
-	folder.broj_datoteka = 4;
-	folder.datoteke = datoteka;
-	strcpy(folder.naziv, "folder1");
-	velicina = velicina_slike(&folder);
-	printf("%.2lf", velicina);
-	getchar();
-	getchar();
-	return 0;
+    double velicina_datoteka;
+    struct datoteka datoteka[]= //upisivanje podataka u strukturu
+    {
+        {"slika1.jpg", 130},
+        {"slika2.jpg", 130},
+        {"text.txt", 13}
+    };
+    struct folder folder[]=     //upisivanje podataka u strukturu strukture
+    {
+        {"root", NULL, 0, datoteka, 3}
+    };
+    velicina_datoteka=moja_funkcija(folder);
+    printf("%0.2lf\n", velicina_datoteka);
+    return 0;
 }
-double velicina_slike(struct folder *pokazivac_na_folder) 
+double moja_funkcija(struct folder *mapa)
 {
-	unsigned ukupna_velicina_slika = 0;
-	int brojac;
-	for (brojac = 0; brojac < pokazivac_na_folder->broj_datoteka; brojac++)
-	{
-		int duljina_naziva = strlen(pokazivac_na_folder->datoteke[brojac].naziv);
-		char *eks = pokazivac_na_folder->datoteke[brojac].naziv + duljina_naziva - 4;
-		if (!strcmp(eks, ".jpg"))
-		{
-			ukupna_velicina_slika += pokazivac_na_folder->datoteke[brojac].velicina;
-			puts(pokazivac_na_folder->datoteke[brojac].naziv);
-		}
-	}
-	return ukupna_velicina_slika;
+    double ukupna_velicina=0;
+    int brojac;
+    char *pointer;
+    for(brojac=0; brojac<3;++brojac)
+    {
+        pointer=strchr(mapa->datoteke[brojac].naziv, '.');  //postavljanje pointera na . jer trebam .jpg
+        if(!strcmp(pointer, ".jpg"))
+        {
+        puts(mapa->datoteke[brojac].naziv);                 //ispis naziva datoteke sa trazenom ekstenzijom
+        ukupna_velicina+=mapa->datoteke[brojac].velicina;   //zbrajanje velicina datoteka
+        }
+    }
+    return ukupna_velicina;
 }
