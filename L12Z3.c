79c77718bad36264fb7c1584ba9f643e436cdb65
Mario Krajacic <krajacic.mario@gmail.com>
2017-05-26 07:36:40 +0200

    Add files via upload

--- L12Z3.c
+++ L12Z3.c
@@ -1,27 +1,26 @@
-/*U tekstualnoj datoteci index.txt zapisani su cijeli brojevi odvojeni razmacima. U binarnoj datoteci podaci.dat zapisane su slijedno vrijednosti tipa double. 
-Ispisati na zaslon sadrûaj datoteke podaci.dat onim redom kojim su njihovi redni brojevi zapisani u datoteci index.txt.
-Npr. za sadrûaj datoteke index.txt: 3 5 1 4 2
-Ispisati na zaslon prvo 3. vrijednost iz datoteke podaci.dat, potom 5., 1., 4., te naposljetku 2. zapisanu vrijednost.*/
+/*U tekstualnoj datoteci index.txt zapisani su cijeli brojevi odvojeni razmacima. U binarnoj datoteci podaci.dat zapisane su slijedno vrijednosti tipa double.
+ Ispisati na zaslon sadr≈æaj datoteke podaci.dat onim redom kojim su njihovi redni brojevi zapisani u datoteci index.txt.
+ Npr. za sadr≈æaj datoteke index.txt: 3 5 1 4 2
+ Ispisati na zaslon prvo 3. vrijednost iz datoteke podaci.dat, potom 5., 1., 4., te naposljetku 2. zapisanu vrijednost.*/
 #include<stdio.h>
 int main(void)
 {
-	FILE *index, *podaci;
-	index = fopen("index.txt", "r");
-	if (index == NULL)
-		return 1;
-	podaci = fopen("podaci.dat", "r");
-	if (podaci == NULL)
-		return 2;
-	double vrijednost_iz_bin_datoteke[5];
-	fread(vrijednost_iz_bin_datoteke, sizeof(double), 5, podaci);
-	int indexi[5];
-	int brojac_indexa = 0;
-	while(fscanf(index, "%d", indexi)!=EOF)
-	{
-		
-
-	}
-	
-
-	return 0;
+    FILE *index, *podaci;
+    float podaci_u_datoteci[]={1.0, 2.0, 3.0, 4.0, 5.0};    //priprema niza za bin datoteku
+    index = fopen("index.txt", "r");
+    if (index == NULL)
+        return 1;
+    podaci = fopen("podaci.dat", "wb+");
+    if (podaci == NULL)
+        return 2;
+    fwrite(&podaci_u_datoteci, sizeof(float), 5, podaci);
+    int indexi=0;
+    while(fscanf(index, "%d", &indexi)!=EOF)        //prolazak kroz txt datoteku i uzimanje indexa
+    {
+        float brojevi;
+        fseek(podaci, sizeof(float)*(indexi-1), SEEK_SET);  //postavljanje na poziciju unutar bin datoteke s koje citam vrijednost
+        fread(&brojevi, sizeof(float), 1, podaci);  //citanje vrijednosti na indexu iz txt
+        printf("%.2lf\n", brojevi);         //ispis
+    }
+    return 0;
 }
