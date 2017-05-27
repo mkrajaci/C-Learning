93bfbc3acc28cc474d0b6709e4054951569d4562
Mario Krajacic <krajacic.mario@gmail.com>
2017-05-27 16:09:09 +0200

    Add files via upload

--- Head.c
+++ Head.c
@@ -6,7 +6,7 @@ int main(void)
     FILE *f;
 //    char putanja[100];
     char buffer[100];
-    int n;
+    int n, brojac_linija=0;
 //    printf("Unesite putanju datoteke:\n");
 //    gets(putanja);
     printf("Unesite koliko linija zelite ispisati:\n");
@@ -15,6 +15,11 @@ int main(void)
     f=fopen("recenice.txt", "r");
     if(f==NULL)
         return 9;
+    while (fgets(buffer, 100, f)!=NULL)
+        ++brojac_linija;    //brojanje ukupnog broja linija
+    fseek(f, 0, SEEK_SET);  //vracanje kursora na pocetak datoteke
+    if(brojac_linija<n)     //Ako je unesen veci broj linija od mogucih ispisa
+        return printf("Unijeli ste veci broj linija za ispis od postojecih.");
     for(int brojac=0; brojac<n; ++brojac)
     {
         fgets(buffer, 100, f);  //citanje linije po linije iz datoteke
