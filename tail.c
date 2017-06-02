88216a1e29f04a6dc6d929d0a22135bdbd08e2f5
Mario Krajacic <krajacic.mario@gmail.com>
2017-06-02 12:26:53 +0200

    Add files via upload

--- tail.c
+++ tail.c
@@ -5,13 +5,13 @@ int main(void)
 {
     FILE *f;
     char buffer[100];
-//    char putanja[100];
+    //    char putanja[100];
     int n;
-//    printf("Unesite putanju do svoje dateke:\n");
-//    gets(putanja);
+    //    printf("Unesite putanju do svoje dateke:\n");
+    //    gets(putanja);
     printf("Unesite koliko zadnjih linija zelite ispisati:\n");
     scanf("%d", &n);
-//    f=fopen(putanja, "r");
+    //    f=fopen(putanja, "r");
     f=fopen("recenice.txt", "r");
     if(f==NULL)
         return 9;
@@ -21,20 +21,24 @@ int main(void)
     for(; brojac>=0; --brojac)
     {
         fseek(f, brojac, SEEK_SET); //pomicanje kursora na slovo po slovo od kraja prema pocetku datoteke
-        if(fgetc(f)=='\n')      //trazenje znaka za novi red
+        if((ftell(f)==0)||(fgetc(f)=='\n'))      //trazenje znaka za novi red
         {
             ++brojac_novih_redova;  //brojanje znakova za novi red ustvari recenica
-            if(brojac_novih_redova>n)   //kada je broj recenica veci od trazenog ispisuje sljedece recenice nakon n
+            if(brojac_novih_redova>n)   //kada je dostignut trazeni broj recenica, break
             {
-                for(brojac=0; brojac<=n; ++brojac)
-                {
-                    if(fgets(buffer, 100, f)!=NULL)//citanje linije po linije iz datoteke
-                    {
-                        puts(buffer);
-                    }
-                    else break;
-                }
+                break;
             }
+        }
+    }
+    if(brojac_novih_redova>=n)   //ispis recenica
+    {
+        for(brojac=0; brojac<=n; ++brojac)
+        {
+            if(fgets(buffer, 100, f)!=NULL) //citanje linije po linije iz datoteke
+            {
+                puts(buffer);
+            }
+            else break;
         }
     }
     fclose(f);
